3b1b7a8877aa2eda8b3e5c6844e993934ad8b6f0
Abhishek Gorale <gorale.abhishek@gmail.com>
2020-12-23 17:00:12 +0530

    Service Layer implemented.

--- KnowYourWeather/src/main/java/com/scania/knowYourWeather/controller/WeatherController.java
+++ KnowYourWeather/src/main/java/com/scania/knowYourWeather/controller/WeatherController.java
@@ -1,20 +1,19 @@
 package com.scania.knowYourWeather.controller;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URLEncoder;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.client.RestTemplate;
 
 import com.scania.knowYourWeather.dto.CurrentWeatherDTO;
+import com.scania.knowYourWeather.service.WeatherService;
 
 @Controller
 public class WeatherController {
@@ -23,36 +22,23 @@ public class WeatherController {
     @Value("${title}")
     private String title;
     
-    @Value("${weatherstack.api.key}")
-    private String apiKey;
-    
-    @Value("${weatherstack.api.url.currentWeather}")
-    private String apiUrlCurrentWeather;
-    
     @Value("#{'${weather.city.list}'.split(',')}") 
     private List<String> cityList;
+    
+    @Autowired
+    private WeatherService weatherService;
 
     @GetMapping("/getCurrentWeather/{cityName}")
     public String getCurrentWeather(@PathVariable(name = "cityName") String cityName,  Model model) throws URISyntaxException, UnsupportedEncodingException {
     	
-    	if(cityName.contains(" ")) {
-    		cityName = URLEncoder.encode(cityName, "UTF-8");
-    	}
-    	
-    	String apiUrl = apiUrlCurrentWeather.replace("YOUR_ACCESS_KEY", apiKey).replace("YOUR_CITY", cityName);
-    	
-    	URI uri = new URI(apiUrl);
-    
-    	RestTemplate restTemplate = new RestTemplate();
-    	
-    	ResponseEntity<CurrentWeatherDTO> response = restTemplate.getForEntity(uri, CurrentWeatherDTO.class);
+    	ResponseEntity<CurrentWeatherDTO> response = weatherService.getCurrentWeather(cityName);
     	
     	model.addAttribute("message", response.getBody().toString());
     	model.addAttribute("title", title); 
     
     	return "welcome";
     }
-    
+
     @GetMapping("/getCityList")
     public String getCityList(Model model) {
     	
--- KnowYourWeather/src/main/java/com/scania/knowYourWeather/service/WeatherService.java
+++ KnowYourWeather/src/main/java/com/scania/knowYourWeather/service/WeatherService.java
@@ -1,9 +1,39 @@
 package com.scania.knowYourWeather.service;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.scania.knowYourWeather.dto.CurrentWeatherDTO;
 
 @Service
 public class WeatherService {
 
+	@Value("${weatherstack.api.key}")
+    private String apiKey;
+    
+    @Value("${weatherstack.api.url.currentWeather}")
+    private String apiUrlCurrentWeather;
 	
+	public ResponseEntity<CurrentWeatherDTO> getCurrentWeather(String cityName)
+			throws UnsupportedEncodingException, URISyntaxException {
+		if(cityName.contains(" ")) {
+    		cityName = URLEncoder.encode(cityName, "UTF-8");
+    	}
+    	
+    	String apiUrl = apiUrlCurrentWeather.replace("YOUR_ACCESS_KEY", apiKey).replace("YOUR_CITY", cityName);
+    	
+    	URI uri = new URI(apiUrl);
+    
+    	RestTemplate restTemplate = new RestTemplate();
+    	
+    	ResponseEntity<CurrentWeatherDTO> response = restTemplate.getForEntity(uri, CurrentWeatherDTO.class);
+		return response;
+	}
 }
